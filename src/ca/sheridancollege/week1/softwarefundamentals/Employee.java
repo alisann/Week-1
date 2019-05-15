0556b467bd671e8f85f01f3249fa38f4edcf2854
alisann <50676860+alisann@users.noreply.github.com>
2019-05-15 15:51:27 -0400

    Update Employee.java

--- src/ca/sheridancollege/week1/softwarefundamentals/Employee.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/Employee.java
@@ -3,6 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
+/*
+ * FIRST COMMENT
+ */
 package ca.sheridancollege.week1.softwarefundamentals;
 
 /**
